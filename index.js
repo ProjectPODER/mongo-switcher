4221085935974e4f85c6de4866b19d9545d34d5c
Fernando Matzdorf <fernando@rindecuentas.org>
2020-06-02 18:57:23 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const MongoClient = require('mongodb').MongoClient;
+const commandLineArgs = require('command-line-args');
+const parseIndexes = require('./lib/parser');
+const createIndex = require('./lib/indexer');
+
+const optionDefinitions = [
+    { name: 'uri', alias: 'u', type: String, defaultValue: 'localhost:27017' },
+    { name: 'database', alias: 'd', type: String },
+    { name: 'collection', alias: 'c', type: String },
+    { name: 'file', alias: 'f', type: String }
+];
+
+const args = commandLineArgs(optionDefinitions);
+if(!args.database || !args.collection) {
+    console.log('ERROR: Please specify both a database and a collection.');
+    process.exit(1);
+}
+if(!args.file) {
+    console.log('ERROR: No index file specified.');
+    process.exit(1);
+}
+
+const db_uri = args.uri;
+const client = new MongoClient(db_uri, {useUnifiedTopology: true});
+
+client.connect(async function(err) {
+    if(err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    console.log("Connected to mongo.");
+
+    const db = client.db(args.database);
+    const collection = db.collection(args.collection);
+
+    let indexes = parseIndexes(args.file);
+
+    for(let i=0; i<indexes.length; i++) {
+        console.log('Creating index ' + indexes[i].name);
+        let result = await createIndex(collection, indexes[i]);
+        console.log(result);
+    }
+
+    client.close();
+    console.log('Done!');
+    process.exit(0);
+});
