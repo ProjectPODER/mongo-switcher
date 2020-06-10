8d0706b8a58269870cf0455c66279c16d5616939
Fernando Matzdorf <fernando@rindecuentas.org>
2020-06-10 18:12:36 -0500

    Fixed mongo uri and environment variable that contains it

--- mongo_remote_switch.sh
+++ mongo_remote_switch.sh
@@ -1,7 +1,7 @@
 # ORIGIN_POD=mongo-0
 # DESTINATION_POD=mongo-0
 # ORIGIN_DATABASE=poppins
-# DESTINATION_DATABASE=poppins 
+# DESTINATION_DATABASE=poppins
 # ORIGIN_COLLECTION=test
 # DESTINATION_COLLECTION=test
 
@@ -12,7 +12,7 @@ source /var/lib/jenkins/allvars
 kubectl port-forward $ORIGIN_POD 27017:27017 &
 PID=$!
 # echo "Running mongo dump"
-mongodump --uri="mongodb://$MONGODB_URI" -d $ORIGIN_DATABASE -c $ORIGIN_COLLECTION /tmp/dumps/$ORIGIN_COLLECTION
+mongodump --uri=mongodb://$POPPINS_MONGO_HOST -d $ORIGIN_DATABASE -c $ORIGIN_COLLECTION /tmp/dumps/$ORIGIN_COLLECTION
 
 kill -9 $PID
 
@@ -23,17 +23,17 @@ kubectl port-forward $DESTINATION_POD 27017:27017 &
 PID=$!
 
 # echo "Running mongo restore"
-mongorestore --uri="mongodb://$MONGODB_URI" -d $DESTINATION_DATABASE -c $ORIGIN_COLLECTION_new /tmp/dumps/$ORIGIN_COLLECTION
+mongorestore --uri=mongodb://$POPPINS_MONGO_HOST -d $DESTINATION_DATABASE -c $ORIGIN_COLLECTION_new /tmp/dumps/$ORIGIN_COLLECTION
 
 rm /tmp/dumps/$ORIGIN_COLLECTION
 
 #echo "DROP old old destination collection"
-mongo $MONGODB_URI --eval "db.${DESTINATION_COLLECTION}_old.drop()"
+mongo $POPPINS_MONGO_HOST --eval "db.${DESTINATION_COLLECTION}_old.drop()"
 
 #echo "Rename current destination collection to old"
-mongo $MONGODB_URI --eval "db.${DESTINATION_COLLECTION}.renameCollection(${DESTINATION_COLLECTION}_old)"
+mongo $POPPINS_MONGO_HOST --eval "db.${DESTINATION_COLLECTION}.renameCollection(${DESTINATION_COLLECTION}_old)"
 
 #echo "Rename new origin collection to destination collection"
-mongo $MONGODB_URI --eval "db.${ORIGIN_COLLECTION}_new.renameCollection(${DESTINATION_COLLECTION})"
+mongo $POPPINS_MONGO_HOST --eval "db.${ORIGIN_COLLECTION}_new.renameCollection(${DESTINATION_COLLECTION})"
 
 kill -9 $PID
