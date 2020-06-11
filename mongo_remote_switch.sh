61a1387d2b91f2f61331e98796a52aedb3389f0d
Martín S <martin@rindecuentas.org>
2020-06-10 21:02:12 -0500

    Some fixes

--- mongo_remote_switch.sh
+++ mongo_remote_switch.sh
@@ -31,9 +31,9 @@ kubectl port-forward $DESTINATION_POD 27017:27017 &
 PID=$!
 
 # echo "Running mongo restore"
-mongorestore --uri=$LOCAL_MONGODB_URI -c="${ORIGIN_COLLECTION}_new" -d=poppins --gzip --dir="/tmp/dumps/poppins/${ORIGIN_COLLECTION}.bson.gz"
+mongorestore --uri=$LOCAL_MONGODB_URI -c="${ORIGIN_COLLECTION}_new" --dump -d=poppins --gzip --dir="/tmp/dumps/poppins/${ORIGIN_COLLECTION}.bson.gz"
 
-rm /tmp/dumps/$ORIGIN_COLLECTION
+rm -rf /tmp/dumps/
 
 #echo "DROP old old destination collection"
 mongo $LOCAL_MONGODB_URI --eval "db.${DESTINATION_COLLECTION}_old.drop()"
