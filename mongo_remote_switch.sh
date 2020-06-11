b379a3286b13c6032a6e6fc63698a6cb576c915b
Martín S <martin@rindecuentas.org>
2020-06-11 11:12:51 -0500

    mongo_remote_swith applies to all collections now

--- mongo_remote_switch.sh
+++ mongo_remote_switch.sh
@@ -2,8 +2,6 @@
 # DESTINATION_POD=mongo-0
 # ORIGIN_DATABASE=poppins
 # DESTINATION_DATABASE=poppins
-# ORIGIN_COLLECTION=test
-# DESTINATION_COLLECTION=[records persons organizations memberships countries]
 
 ENVIRONMENT=staging
 
@@ -19,7 +17,11 @@ kubectl port-forward $ORIGIN_POD 27017:27017 &
 PID=$!
 
 # echo "Running mongo dump"
-mongodump --uri=$LOCAL_MONGODB_URI -c=$ORIGIN_COLLECTION --gzip --out=/tmp/dumps/
+dump("records")
+dump("persons")
+dump("organizations")
+dump("memberships")
+dump("countries")
 
 kill -9 $PID
 
@@ -30,18 +32,31 @@ source /var/lib/jenkins/allvars
 kubectl port-forward $DESTINATION_POD 27017:27017 &
 PID=$!
 
-# echo "Running mongo restore"
-mongorestore --uri=$LOCAL_MONGODB_URI -c="${ORIGIN_COLLECTION}_new" --drop -d=poppins --gzip --dir="/tmp/dumps/poppins/${ORIGIN_COLLECTION}.bson.gz"
+restore("records")
+restore("persons")
+restore("organizations")
+restore("memberships")
+restore("countries")
 
-rm -rf /tmp/dumps/
-
-#echo "DROP old old destination collection"
-mongo $LOCAL_MONGODB_URI --eval "db.${DESTINATION_COLLECTION}_old.drop()"
-
-#echo "Rename current destination collection to old"
-mongo $LOCAL_MONGODB_URI --eval "db.${DESTINATION_COLLECTION}.renameCollection('${DESTINATION_COLLECTION}_old')"
-
-#echo "Rename new origin collection to destination collection"
-mongo $LOCAL_MONGODB_URI --eval "db.${ORIGIN_COLLECTION}_new.renameCollection('${DESTINATION_COLLECTION}')"
 
 kill -9 $PID
+
+function dump {
+    mongodump --uri=$LOCAL_MONGODB_URI -c=$1 --gzip --out=/tmp/dumps/
+}
+
+function restore {
+    # echo "Running mongo restore"
+    mongorestore --uri=$LOCAL_MONGODB_URI -c="${1}_new" --drop -d=$DESTINATION_DATABASE --gzip --dir="/tmp/dumps/${ORIGIN_DATABASE}/${COLLECTION}.bson.gz"
+
+    rm -rf /tmp/dumps/
+
+    #echo "DROP old old destination collection"
+    mongo $LOCAL_MONGODB_URI --eval "db.${1}_old.drop()"
+
+    #echo "Rename current destination collection to old"
+    mongo $LOCAL_MONGODB_URI --eval "db.${1}.renameCollection('${1}_old')"
+
+    #echo "Rename new origin collection to destination collection"
+    mongo $LOCAL_MONGODB_URI --eval "db.${1}_new.renameCollection('${1}')"
+}
