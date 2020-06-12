02928ab4d1aa4e60871de83a9cef91e5f6bbef25
Martín S <martin@rindecuentas.org>
2020-06-12 14:33:41 -0500

    Separate orign and destination mongodb uri

--- mongo_remote_switch.sh
+++ mongo_remote_switch.sh
@@ -1,17 +1,13 @@
-# ORIGIN_POD=mongo-0
+# ORIGIN_POD=fast-poppins
 # DESTINATION_POD=mongo-0
 # ORIGIN_DATABASE=poppins
 # DESTINATION_DATABASE=poppins
+# ORIGIN_MONGODB_URI=mongodb://localhost:27017
+# DESTINATION_MONGODB_URI=mongodb://user:pass@mongo-0.mongo:27017
 
 ENVIRONMENT=staging
 
 source /var/lib/jenkins/allvars
-
-MONGODB_URI="$(kubectl get secret mongodb-uri -o=jsonpath --template={.data.MONGODB_URI} | base64 --decode)"
-MONGODB_CLUSTER_HOST="mongo-0.mongo.default.svc.cluster.local,mongo-1.mongo.default.svc.cluster.local"
-REPSET="?replicaSet=MainRepSet"
-LOCAL_MONGODB_URI=${MONGODB_URI/$MONGODB_CLUSTER_HOST/localhost}
-LOCAL_MONGODB_URI=${LOCAL_MONGODB_URI/$REPSET/}
 
 kubectl port-forward $ORIGIN_POD 27017:27017 &
 PID=$!
@@ -42,21 +38,21 @@ restore("countries")
 kill -9 $PID
 
 function dump {
-    mongodump --uri=$LOCAL_MONGODB_URI -c=$1 --gzip --out=/tmp/dumps/
+    mongodump --uri=$ORIGIN_MONGODB_URI -c=$1 --gzip --out=/tmp/dumps/
 }
 
 function restore {
     # echo "Running mongo restore"
-    mongorestore --uri=$LOCAL_MONGODB_URI -c="${1}_new" --drop -d=$DESTINATION_DATABASE --gzip --dir="/tmp/dumps/${ORIGIN_DATABASE}/${COLLECTION}.bson.gz"
+    mongorestore --uri=$DESTINATION_MONGODB_URI -c="${1}_new" --drop -d=$DESTINATION_DATABASE --gzip --dir="/tmp/dumps/${ORIGIN_DATABASE}/${COLLECTION}.bson.gz"
 
     rm -rf /tmp/dumps/
 
     #echo "DROP old old destination collection"
-    mongo $LOCAL_MONGODB_URI --eval "db.${1}_old.drop()"
+    mongo $DESTINATION_MONGODB_URI --eval "db.${1}_old.drop()"
 
     #echo "Rename current destination collection to old"
-    mongo $LOCAL_MONGODB_URI --eval "db.${1}.renameCollection('${1}_old')"
+    mongo $DESTINATION_MONGODB_URI --eval "db.${1}.renameCollection('${1}_old')"
 
     #echo "Rename new origin collection to destination collection"
-    mongo $LOCAL_MONGODB_URI --eval "db.${1}_new.renameCollection('${1}')"
+    mongo $DESTINATION_MONGODB_URI --eval "db.${1}_new.renameCollection('${1}')"
 }
