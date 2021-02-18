8401b3223ea102363046c44faf045c33d1643bba
Martín S <martin@rindecuentas.org>
2021-02-18 11:55:16 -0600

    New version using a droplet as intermediary (previously elastic-switcher)

---
+++ launch_droplet.sh
@@ -0,0 +1,32 @@
+source env_vars
+
+if [[ ! -v DOCTL_TOKEN ]]; then
+    echo "DOCTL_TOKEN missing from env"
+    exit 1
+fi 
+if [[ ! -v SSH_KEYS ]]; then
+    echo "SSH_KEYS missing from env"
+    exit 1
+fi 
+
+
+echo "Creating ${SCRIPT_LABEL}${YEAR} droplet ..."
+RESPONSE=`doctl compute droplet create --region ${DROPLET_REGION} --size ${DROPLET_SIZE} --image ${DROPLET_IMAGE} --tag-name ${SCRIPT_LABEL} --wait --format PublicIPv4 --ssh-keys=\"${SSH_KEYS}\" ${SCRIPT_LABEL}`
+IP="${RESPONSE/Public IPv4$'\n'/}"
+sleep 20
+
+echo "Configuring ${IP} ..."
+ssh  -o "StrictHostKeyChecking no" root@${IP} "wget https://github.com/digitalocean/doctl/releases/download/v1.54.0/doctl-1.54.0-linux-amd64.tar.gz && tar xf ~/doctl-1.54.0-linux-amd64.tar.gz && sudo mv ~/doctl /usr/local/bin && mkdir -p ~/.config/doctl/ && echo \"access-token: ${DOCTL_TOKEN}\" > .config/doctl/config.yaml && doctl kubernetes cluster kubeconfig save k8s-production && doctl kubernetes cluster kubeconfig save k8s-1-14-4-do-0-sfo2-1563844775684"
+
+echo "Copying script to ${IP} ..."
+ssh  root@${IP} mkdir -p $SCRIPT_PATH_REMOTE
+scp -rC $SCRIPT_PATH_LOCAL root@${IP}:$SCRIPT_PATH_REMOTE
+scp -rC allvars root@${IP}:$SCRIPT_PATH_REMOTE
+
+echo "Launching mongo_remote_switch in ${IP} ..."
+
+ssh root@${IP} "cd $SCRIPT_PATH_REMOTE && bash -ex ./mongo_remote_switch.sh"
+
+echo "Deleting ${IP}"
+doctl compute droplet delete --force --tag-name ${SCRIPT_LABEL}
+ssh-keygen -f "/var/lib/jenkins/.ssh/known_hosts" -R "${IP}"
--- mongo_remote_switch.sh
+++ mongo_remote_switch.sh
@@ -8,34 +8,47 @@
 
 
 function dump {
+    echo "mongo dump ${ORIGIN_POD} ${1}"
     kubectl exec -it $ORIGIN_POD -- mongodump --uri=$ORIGIN_MONGODB_URI -c=$1 --gzip --out=/data/db/switcher/
 }
 
 function restore {
-    # echo "Running mongo restore"
+    echo "Drop old new collection ${DESTINATION_POD} ${1}_new"
+    kubectl exec -it $DESTINATION_POD -- mongo $DESTINATION_MONGODB_URI --eval "db.${1}_new.drop()"
+
+    echo "Running mongo restore ${DESTINATION_POD} ${1}"
     kubectl exec -it $DESTINATION_POD -- mongorestore --uri=$DESTINATION_MONGODB_URI -c="${1}_new" --drop -d=$DESTINATION_DATABASE --gzip --dir="/data/db/switcher/${ORIGIN_DATABASE}/${1}.bson.gz"
 
-    #echo "DROP old old destination collection"
-    kubectl exec -it $DESTINATION_POD -- mongo $DESTINATION_MONGODB_URI --eval "db.${1}_old.drop()"
+    if [ "$NOT_TESTING_MONGO" = 'true' ]; then
+        echo "Not testing mongo, proceed with destructive actions in production."
+        
+        echo "DROP old old destination collection"
+        kubectl exec -it $DESTINATION_POD -- mongo $DESTINATION_MONGODB_URI --eval "db.${1}_old.drop()"
 
-    #echo "Rename current destination collection to old"
-    kubectl exec -it $DESTINATION_POD -- mongo $DESTINATION_MONGODB_URI --eval "db.${1}.renameCollection('${1}_old')"
+        echo "Rename current destination collection to old"
+        kubectl exec -it $DESTINATION_POD -- mongo $DESTINATION_MONGODB_URI --eval "db.${1}.renameCollection('${1}_old')"
 
-    #echo "Rename new origin collection to destination collection"
-    kubectl exec -it $DESTINATION_POD -- mongo $DESTINATION_MONGODB_URI --eval "db.${1}_new.renameCollection('${1}')"
+        echo "Rename new origin collection to destination collection"
+        kubectl exec -it $DESTINATION_POD -- mongo $DESTINATION_MONGODB_URI --eval "db.${1}_new.renameCollection('${1}')"
+    else
+        echo "Testing mongo. Skipping destructive actions."
+    fi 
 }
 
 
+echo "Start mongo_remote_switch"
+
 ENVIRONMENT=staging
 
-source /var/lib/jenkins/allvars
+source allvars
+source env_vars
 
 # echo "Running mongo dump"
 dump "records" 
-dump "persons"
-dump "organizations"
-dump "memberships"
-dump "countries"
+#dump "persons"
+#dump "organizations"
+#dump "memberships"
+#dump "countries"
 
 kubectl cp $ORIGIN_POD:/data/db/switcher/ /tmp/dumps/
 
@@ -44,9 +57,11 @@ kubectl config use-context $PRODUCTION
 kubectl cp /tmp/dumps/ $DESTINATION_POD:/data/db/switcher/ 
 
 restore "records"
-restore "persons"
-restore "organizations"
-restore "memberships"
-restore "countries"
+#restore "persons"
+#restore "organizations"
+#restore "memberships"
+#restore "countries"
 
 rm -rf /tmp/dumps/
+
+echo "Finished mongo_remote_switch"
